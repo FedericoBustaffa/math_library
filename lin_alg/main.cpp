b0aca109c75424242ac14c7eacf51ac6a7b96176
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-02-01 09:58:05 +0100

    empty

--- lin_alg/main.cpp
+++ lin_alg/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, const char **argv)
 {
-	matrix<double> a = matrix<double>::rand_real(dim, dim, 0, 5);
-	matrix<double> b = matrix<double>::rand_real(dim, dim, 0, 5);
-	matrix<double> c(dim, dim);
-	c = a * b;
+    matrix<double> a = matrix<double>::rand_real(dim, dim, 0, 5);
+    matrix<double> b = matrix<double>::rand_real(dim, dim, 0, 5);
+    matrix<double> c(dim, dim);
+    c = a * b;
 
-	return 0;
+    return 0;
 }
