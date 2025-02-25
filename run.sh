c800a8f33e427503dcbda0670d6b28d39be74e3b
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-02-25 14:48:17 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-cd build/
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-./test $1
-cd ..
+
+if [ $# -eq 0 ]; then
+	bt=Debug
+else
+	bt=$1
+fi
+
+cd ./build/
+cmake .. -DCMAKE_BUILD_TYPE=$bt
+
+if make; then
+	cd ..
+	if [ $bt == "Debug" ]; then
+		valgrind -s --tool=cachegrind ./build/test $2
+	else
+		./build/test $2
+	fi
+else
+	echo "compilation failed"
+	exit 1
+fi
